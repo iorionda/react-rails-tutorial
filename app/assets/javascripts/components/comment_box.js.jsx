09586ac6d88d2716afdedec9ebac9dbd6c163808
Iori ONDA <iorionda@gmail.com>
2015-10-30 13:15:48 +0900

    Modify CommentList

--- app/assets/javascripts/components/comment_box.js.jsx
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -2,8 +2,30 @@ var CommentBox = React.createClass({
   render: function() {
     return (
        <div className="commentBox" >
-          Hello, world! I am CommentBox.
+          <h1>CommentBox</h1>
+          <CommentList />
+          <CommentForm />
        </div>
     );
   }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+            <div className="commentForm">
+        Hello, world! I am CommentForm.
+        </div>
+        );
+    }
 });
