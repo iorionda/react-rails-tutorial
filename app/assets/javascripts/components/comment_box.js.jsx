b477340f21a064a34d909b3fcfeda9fb2458e9d1
Iori ONDA <iorionda@gmail.com>
2015-10-30 13:32:47 +0900

    Use comment in commentList

--- app/assets/javascripts/components/comment_box.js.jsx
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -11,34 +11,35 @@ var CommentBox = React.createClass({
 });
 
 var CommentList = React.createClass({
-  render: function() {
-    return (
-      <div className="commentList">
-        Hello, world! I am CommentList.
-      </div>
-    );
-  }
+    render: function() {
+        return (
+            <div className="commentList">
+                <Comment author="Pete Hunt">This is one comment</Comment>
+                <Comment author="Jordan Walke">This is *author* comment</Comment>
+            </div>
+        );
+    }
 });
 
 var CommentForm = React.createClass({
     render: function() {
         return (
             <div className="commentForm">
-        Hello, world! I am CommentForm.
-        </div>
+                Hello, world! I am CommentForm.
+            </div>
         );
     }
 });
 
 var Comment = React.createClass({
-  render: function() {
-    return (
-      <div className="comment">
-         <h2 classNmae="commentAuthor" >
-           {this.prop.author}
-        </h2>
-           {this.props.children}
-        </div>
-    );
-  }
-})
+    render: function() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                {this.props.children}
+            </div>
+        );
+    }
+});
